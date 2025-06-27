3cd2dbab6e1c865caa49beabb12b6b877637b83c
Umpi .... <neldis537@gamil.com>
2025-06-27 16:51:03 -0400

    cambiamos la base de datos

--- config/database.ts
+++ config/database.ts
@@ -33,7 +33,7 @@ export default ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'tienda-dev'),
+        database: env('DATABASE_NAME', 'strapi-backend'),
         user: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false) && {

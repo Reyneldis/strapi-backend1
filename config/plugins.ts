3494fb9d2593864cf2ea6ab389dcee87eb9a3915
Umpi .... <neldis537@gamil.com>
2025-07-02 09:05:20 -0400

    modified: config/database.ts,  config/plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-supabase', // Nombre completo del paquete
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'), // Solo el nombre de la variable, no la URL completa
+        apiKey: env('SUPABASE_API_KEY'), // Solo el nombre de la variable
+        bucket: env('SUPABASE_BUCKET', 'strapi-uploads'), // Valor por defecto
+        directory: env('SUPABASE_DIRECTORY', ''), // Directorio dentro del bucket, no URL
+        options: {},
+      },
+    },
+  },
+});

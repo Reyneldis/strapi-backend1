c786a0aa27e03d1591910662881419839739c802
Umpi .... <neldis537@gamil.com>
2025-07-02 09:33:34 -0400

    modified:   config/server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 4000),
-  url: env('PUBLIC_URL', 'https://strapi-backend1-enxe.onrender.com'),
+  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
